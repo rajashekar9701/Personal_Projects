9d0de2b863a274417470a06c6202a4d3c04a7cfb
Rajashekar Kandi <rajashekar_kandi@persistent.com>
2024-03-04 15:05:08 +0530

    personal_pro

---
+++ node Project/products.js
@@ -0,0 +1,27 @@
+const products=[
+    {
+        id:1,
+        name:"iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/dhe91txr5/image/upload/v1702963890/online-shop/galaxyS_larypj.png"
+    },
+    {
+        id:2,
+        name:"iPhone 12",
+        desc:"5.1-inch display",
+        price:899,
+        image:"https://res.cloudinary.com/dhe91txr5/image/upload/v1702963889/online-shop/iphone12pro_oniim8.jpg"
+    },
+    {
+        id:3,
+        name:"iPhone 12 Plus",
+        desc:"7.1-inch display",
+        price:1099,
+        image:"https://res.cloudinary.com/dhe91txr5/image/upload/v1702963889/online-shop/iphone12_ed3tf0.jpg"
+    },
+];
+module.exports=products;
+
+
+
